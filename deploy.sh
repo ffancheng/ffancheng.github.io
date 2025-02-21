39603f5fb8d9ac3fbb2b05508197d4988ff2a105
Fan Cheng <fan.cheng@monash.edu>
2025-02-21 22:27:09 +1100

    TRY fix deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # This script allows you to easily and quickly generate and deploy your website
 # using Hugo to your personal GitHub Pages repository. This script requires a
-# certain configuration, run the `setup.sh` script to configure this. See
+# certain configuration. Run the `setup.sh` script to configure this. See
 # https://hjdskes.github.io/blog/update-deploying-hugo-on-personal-github-pages/
 # for more information.
 
@@ -16,25 +16,33 @@ msg() {
     printf "\033[1;32m :: %s\n\033[0m" "$1"
 }
 
+# Check if there are any uncommitted changes.
 if [[ $(git status -s) ]]; then
     msg "The working directory is dirty, please commit or stash any pending changes"
     exit 1;
 fi
 
-msg "Removing the old website"
-pushd public
-git rm -rf *
-popd
+# Ensure the `public` directory is clean before building.
+msg "Removing the old website (if exists)"
+rm -rf public
 
+# Build the website using Hugo.
 msg "Building the website"
 hugo
 
+# Copy the CNAME file to the `public` directory if it exists.
 msg "Copying CNAME"
 cp CNAME public/
 
+# Navigate to the `public` directory and initialize git.
 msg "Pushing the updated \`public\` folder to the \`gh-pages\` branch"
 pushd public
-git add *
-git commit
+git init
+git add .
+git commit -m "Deploy Hugo site"
 popd
-git push origin gh-pages
+
+# Push the changes to the `gh-pages` branch.
+git subtree push --prefix public origin gh-pages
+
+msg "Deployment complete!"
